740988d65934ecd5b0369cb25dacb03aff125568
Reedfox100 <reed.fox100@gmail.com>
2023-09-18 18:57:51 -0500

    Error with mutables, calculation working.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use rand::{
 };
 use std::{mem, ptr::null, io};
 
-fn deal_hands(player_count: i32, mut hands: [&mut Hand; 5], deck: &mut Deck) {
+
+fn num_players<'a>(player_count: i32, mut hands: [&'a mut Hand; 5], deck: &'a mut Deck) -> [&'a mut Hand; 5]{
     //shuffle deck and deal hands to each player, including house (currently only 1).
     //future for statement to iterate through players and deal handsdeal hands.
     if player_count == 1 {
@@ -35,12 +36,32 @@ fn deal_hands(player_count: i32, mut hands: [&mut Hand; 5], deck: &mut Deck) {
             deck.deal_to_hand(&mut hands[0], 1);
         }
     }
-    let mut i = 1;
-    while i < 5 && !(hands[i].cards().is_empty()){
-        println!("Hand: {}", hands[i]);
-        i += 1;
+
+    hands
     }
+
+fn calculate_hand(hand: &mut Hand) -> i32{
+    let mut result: i32 = 0;
+    for x in &mut hand.cards{
+        println!("Card: {}", x);
+        match x.rank.to_str(){
+            "Two" =>result += 2,
+            "Three" =>result += 3,
+            "Four" =>result += 4,
+            "Five" =>result += 5,
+            "Six" =>result += 6,
+            "Seven" =>result += 7,
+            "Eight" =>result += 8,
+            "Nine" =>result += 9,
+            "Ten"|"King"|"Queen"|"Jack" =>result += 10,
+            "Ace" =>result += 11,
+            _ =>result += 1,
+        };
+    }
+
+result
 }
+
 
 fn main() {
     // enum Values{
@@ -73,53 +94,40 @@ fn main() {
 
     //Make deck, player, and house (increasing number of max players later)
     let mut deck = Deck::new();
+    deck.shuffle();
     let player_count = 2; //Temporary number until input is added
     let mut player1 = Hand::new();
     let mut player2 = Hand::new();
     let mut player3 = Hand::new();
     let mut player4 = Hand::new();
     let mut house = Hand::new();
-    let hands: [&mut Hand; 5] = [
+    let mut hands: [&mut Hand; 5] = [
         &mut house,
         &mut player1,
         &mut player2,
         &mut player3,
         &mut player4,
     ];
-    deck.shuffle();
+    //Returns the number of players and fills initial hands
+    hands = num_players(player_count, hands, &mut deck);
 
-    deal_hands(player_count, hands, &mut deck)
-    // let ace_of_spades = Card{
-    //     value: Values::Ace("Ace".to_string(), [1,11]),
-    //     suit: Suits::Spades("Spades".to_string()),
-    // };
+    let player_number: i8 = 0;
+    for x in hands{
+        if !(x.cards.is_empty()){
+            if player_number != 0{
+            println!("House: {}({})     ", x, calculate_hand(x));
+            }
+            else{
 
-    // deck.push(ace_of_spades);
-    // for x in deck{
-    //     println!("Card: {} {}", x.0, x.1);
+            }
+
+        }
+    }
+    // let mut input = String::new();
+    // let mut i = 1;
+    // while i < 5 && !(hands[i].cards().is_empty()){
+    //     println!("Hand: {}", hands[i]);
+    //     i += 1;
     // }
 }
 
-// let mut deck: Vec<String> = Vec::new();
-// for x in VALUES_IN_DECK{
-//     for y in SUITES_IN_DECK{
-//         let card_pieces = [x, "of", y];
-//         let card = card_pieces.join(" ");
-//         deck.push(card);
-//     }
-
-// }
-// for x in values_iter {
-//     for y in suites_iter {
-//         let card_pieces = [x, y];
-//         let card = card_pieces.join(" ");
-//         deck.push(card);
-//     }
-// }
-
-// }
-// let mut rng = rand::thread_rng();
-// deck.shuffle(&mut rng);
-// for x in deck{
-//     println!("Card: {}", x);
-// }
